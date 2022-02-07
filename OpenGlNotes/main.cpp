ee54c30a2200b2f20c9bd0cb824f882e19f8b5d2
Jie <chenjieva@gmail.com>
2022-02-06 22:29:48 -0500

    OpenGL Notes
    OpenGL Notes

--- OpenGlNotes/main.cpp
+++ OpenGlNotes/main.cpp
@@ -4,11 +4,61 @@
 //
 //  Created by Jie Chen on 2022-02-05.
 //
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    if(glewInit() != GLEW_OK)
+        std::cout << "glew Init failed" << std::endl;
+    
+    float positions[6] = {
+        -0.5f, 0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
